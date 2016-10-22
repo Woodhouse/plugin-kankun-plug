20ac50d0b1971d60a6281ee543ada4bb5770e0b8
Luke Bonaccorsi <luke@lukeb.co.uk>
2016-10-22 20:16:28 +0100

    Refactor for v2 of woodhouse

--- index.js
+++ index.js
@@ -1,214 +1,188 @@
-var http = require('http');
-var moment = require('moment');
-var dgram = require('dgram');
+'use strict';
 
-var kankun = function(){
-    this.name = 'kankun-plug';
-    this.displayname = 'Kankun Plug';
-    this.description = 'Control your Kankun plugs';
-}
+const bluebird = require('bluebird');
+const request = require('request-promise');
+const moment = require('moment');
 
-kankun.prototype.init = function(){
-    var self = this;
-    this.server = dgram.createSocket('udp4');
-    this.plugs = {};
+class kankun {
+    constructor() {
+        this.name = 'kankun-plug';
+        this.displayname = 'Kankun Plug';
+        this.description = 'Control your Kankun plugs';
+    }
 
-    this.server.bind(9600);
+    init() {
+        this.plugs = {};
 
-    this.server.on('message', function (message) {
-        if (message.toString().match('"source":"kankun-plug"')) {
-            var obj = JSON.parse(message.toString()),
-                tags = [];
+        this.listenBroadcast('kankun-plug', (message) => {
+            let tags;
 
-            if (obj.uuid) {
-                if (obj.group) {
-                    tags = obj.group.split(',')
+            if (message.uuid) {
+                if (message.group) {
+                    tags = message.group.split(',')
                 } else {
-                    tags = obj.tags.split(',');
+                    tags = message.tags.split(',');
                 }
 
                 if (tags instanceof Array === false) {
                     tags = [tags];
                 }
 
-                tags = tags.map(function(value) {
+                tags = tags.map((value) => {
                     return value.trim().toLowerCase();
                 });
 
-                self.plugs[obj.uuid] = {name: obj.name.toLowerCase(), tags: tags, ip: obj.ip};
+                this.plugs[message.uuid] = {name: message.name.toLowerCase(), tags: tags, ip: message.ip};
             }
-        }
-    });
-
-    this.listen('(kankun|small k) on (:<group or item>.+?)', 'standard', function(from, interface, params){
-        self.checkGroups(params[1], function(obj){
-            self.turnOn(obj, interface, from);
         });
-    });
 
-    this.listen('(kankun|small k) off (:<group or item>.+?)', 'standard', function(from, interface, params){
-        self.checkGroups(params[1], function(obj){
-            self.turnOff(obj, interface, from);
+        this.listen('(kankun|small k) on (:<group or item>.+?)', 'standard', (from, interfaceName, params) => {
+            return this.checkGroups(params[1], (obj) => {
+                return this.turnOn(obj, interfaceName, from);
+            });
         });
-    });
 
-    this.listen('(kankun|small k) status (:<group or item>.+?)', 'standard', function(from, interface, params){
-        self.checkGroups(params[1], function(obj){
-            self.checkStatus(obj, interface, from);
+        this.listen('(kankun|small k) off (:<group or item>.+?)', 'standard', (from, interfaceName, params) => {
+            return this.checkGroups(params[1], (obj) => {
+                return this.turnOff(obj, interfaceName, from);
+            });
         });
-    });
 
-    this.listen('(kankun|small k) timer (:<unit>.+?) (:<seconds, minutes, hours or days>second|minute|hour|day|seconds|minutes|hours|days) (:<state>off|on) (:<group or item>.+?)', 'standard',
-        function(from, interface, params){
-            self.setTimer(from, interface, params);
-        }
-    );
+        this.listen('(kankun|small k) status (:<group or item>.+?)', 'standard', (from, interfaceName, params) => {
+            return this.checkGroups(params[1], (obj) => {
+                return this.checkStatus(obj, interfaceName, from);
+            });
+        });
 
-    this.listen('(kankun|small k) cancel timer (:<timer id>.+?)', 'standard',
-        function(from, interface, params){
-            self.cancelTimer(from, interface, params);
-        }
-    );
+        this.listen('(kankun|small k) timer (:<unit>.+?) (:<seconds, minutes, hours or days>second|minute|hour|day|seconds|minutes|hours|days) (:<state>off|on) (:<group or item>.+?)', 'standard',
+            (from, interfaceName, params) => {
+                const crontime = moment().add(params[1], params[2]).toDate();
 
-    this.listen('(kankun|small k) list', 'standard',
-        function(from, interface, params){
-            var tags = {},
-                names = [],
-                message = '';
-
-            for (var key in self.plugs) {
-                names.push(self.plugs[key].name + ' (IP: ' + self.plugs[key].ip + ')');
-                self.plugs[key].tags.forEach(function(value) {
-                    tags[value] = true;
+                return this.addCronJob(crontime, 'timer', {
+                    plug: params[4],
+                    status: params[3],
+                    interfaceName: interfaceName,
+                    from: from
+                }).then((id) => {
+                    return `Timer added with ID ${id}`;
                 });
             }
+        );
 
-            if (names.length > 0) {
-                names = '    - ' + names.join('\n    - ');
-                message += 'Plug names:\n' + names + '\n';
+        this.listen('(kankun|small k) cancel timer (:<timer id>.+?)', 'standard',
+            (from, interfaceName, params) => {
+                return this.removeCronJob(params[1]).then(() => {
+                    return `Timer with ID ${params[1]} cancelled`;
+                });
             }
+        );
 
-            if (Object.keys(tags).length > 0) {
-                tags = Object.keys(tags);
-                tags = '    - ' + tags.join('\n    - ');
-                message += 'Tags:\n' + tags;
+        this.listen('(kankun|small k) list', 'standard',
+            (from, interfaceName, params) => {
+                let tags = {};
+                let names = [];
+                let message = '';
+
+                for (let key in this.plugs) {
+                    names.push(`${this.plugs[key].name} (IP: ${this.plugs[key].ip})`);
+                    this.plugs[key].tags.forEach((value) => {
+                        tags[value] = true;
+                    });
+                }
+
+                if (names.length > 0) {
+                    names = `    - ${names.join('\n    - ')}`;
+                    message += `Plug names:\n${names}\n`;
+                }
+
+                if (Object.keys(tags).length > 0) {
+                    tags = Object.keys(tags);
+                    tags = `    - ${tags.join('\n    - ')}`;
+                    message += `Tags:\n${tags}`;
+                }
+
+                message = message.trim();
+
+                if (message.length === 0) {
+                    message = 'No plugs detected yet'
+                }
+
+                return message;
             }
+        );
 
-            message = message.trim()
-
-            if (message.length === 0) {
-                message = 'No plugs detected yet'
-            }
-
-            self.sendMessage(message, interface, from);
-        }
-    );
-
-    this.registerCronHandler('timer', function(params){
-        self.checkGroups(params.plug, function(obj){
-            if (params.status === 'on') {
-                self.turnOn(obj, params.interface, params.from);
-            } else {
-                self.turnOff(obj, params.interface, params.from);
-            }
+        this.registerCronHandler('timer', (params) => {
+            this.checkGroups(params.plug, (obj) => {
+                if (params.status === 'on') {
+                    this.turnOn(obj, params.interfaceName, params.from);
+                } else {
+                    this.turnOff(obj, params.interfaceName, params.from);
+                }
+            });
         });
-    });
-}
+    }
 
-kankun.prototype.checkGroups = function(name, callback) {
-    for (var key in this.plugs) {
-        if (this.plugs[key].name === name.toLowerCase() || this.plugs[key].tags.indexOf(name.toLowerCase()) > -1) {
-            callback(this.plugs[key]);
+    checkGroups(name, callback) {
+        const promises = [];
+
+        for (let key in this.plugs) {
+            if (this.plugs[key].name === name.toLowerCase() || this.plugs[key].tags.indexOf(name.toLowerCase()) > -1) {
+                promises.push(callback(this.plugs[key]));
+            }
+        }
+        if (promises.length > 0) {
+            return bluebird.all(promises).then((messages) => {
+                return messages.join(`\n`);
+            });
+        } else {
+            return `No plugs matched "${name}"`;
         }
     }
-}
 
-kankun.prototype.turnOn = function(obj, interface, from){
-    var self = this;
-    var options = {
-        hostname: obj.ip,
-        path: '/cgi-bin/relay.cgi?on',
-        headers: {
-            'user-agent': 'Woodhouse Bot - https://github.com/Woodhouse-bot/woodhouse'
-        }
-    };
-    var req = http.get(options, function(res) {
-        self.sendMessage(obj.name + ' has been turned on', interface, from);
-    }).on('error', function(e) {
-      console.log('problem with request: ' + e.message);
-    });
-
-}
-
-kankun.prototype.turnOff = function(obj, interface, from){
-    var self = this;
-    var options = {
-        hostname: obj.ip,
-        path: '/cgi-bin/relay.cgi?off',
-        headers: {
-            'user-agent': 'Woodhouse Bot - https://github.com/Woodhouse-bot/woodhouse'
-        }
-    };
-    var req = http.get(options, function(res) {
-        self.sendMessage(obj.name + ' has been turned off', interface, from);
-    }).on('error', function(e) {
-      console.log('problem with request: ' + e.message);
-    });
-
-}
-
-kankun.prototype.checkStatus = function(obj, interface, from){
-    var self = this;
-    var options = {
-        hostname: obj.ip,
-        path: '/cgi-bin/relay.cgi?state',
-        headers: {
-            'user-agent': 'Woodhouse Bot - https://github.com/Woodhouse-bot/woodhouse'
-        }
-    };
-    var data = '';
-    var req = http.request(options, function(res) {
-        res.on('data', function (response) {
-            data += String(response);
-        });
-
-        res.on('end', function(){
-            if (data.trim() === 'OFF') {
-                self.sendMessage(obj.name + ' is off', interface, from);
-            } else if (data.trim() === 'ON') {
-                self.sendMessage(obj.name + ' is on', interface, from);
+    turnOn(obj, interfaceName, from){
+        return request({
+            uri: `http://${obj.ip}/cgi-bin/relay.cgi?on`,
+            method: `GET`,
+            headers: {
+                'User-Agent': 'Woodhouse Bot - https://github.com/Woodhouse/core'
             }
-        })
-    });
+        }).then((res) => {
+            return `${obj.name} has been turned on`;
+        }).catch((e) => {
+            return `${obj.name} could not be turned on: ${e.message}`;
+        });
+    }
 
-    req.on('error', function(e) {
-      console.log('problem with request: ' + e.message);
-    });
+    turnOff(obj, interfaceName, from) {
+        return request({
+            uri: `http://${obj.ip}/cgi-bin/relay.cgi?off`,
+            method: `GET`,
+            headers: {
+                'User-Agent': 'Woodhouse Bot - https://github.com/Woodhouse/core'
+            }
+        }).then((res) => {
+            return `${obj.name} has been turned off`;
+        }).catch((e) => {
+            return `${obj.name} could not be turned off: ${e.message}`;
+        });
+    }
 
-    req.end();
-}
-
-kankun.prototype.setTimer = function(from, interface, params) {
-    var self = this;
-    var crontime = moment().add(params[1], params[2]).toDate();
-
-    var id = this.addCronJob(crontime, 'timer', {
-        plug: params[4],
-        status: params[3],
-        interface: interface,
-        from: from
-    });
-
-    this.sendMessage('Timer added with ID ' + id, interface, from);
-}
-
-kankun.prototype.cancelTimer = function(from, interface, params) {
-    this.removeCronJob(params[1]);
-    this.sendMessage('Timer with ID ' + params[1] + ' cancelled', interface, from);
-}
-
-kankun.prototype.exit = function() {
-    this.server.close();
-}
+    checkStatus(obj, interfaceName, from) {
+        return request({
+            uri: `http://${obj.ip}/cgi-bin/relay.cgi?state`,
+            method: `GET`,
+            headers: {
+                'User-Agent': 'Woodhouse Bot - https://github.com/Woodhouse/core'
+            }
+        }).then((res) => {
+            if (data.trim() === 'OFF') {
+                return `${obj.name} is off`;
+            } else if (data.trim() === 'ON') {
+                return `${obj.name} is on`;
+            }
+        }).catch((e) => {
+            return `${obj.name} could not be turned reached: ${e.message}`;
+        });
+    }}
 
 module.exports = kankun;
